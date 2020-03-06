b0d1466a752f6dce53fd5a739b428a57929a39bb
etgreer91 <etgreer91@gmail.com>
2020-03-05 23:45:54 -0700

    adds pair plus payout and organization

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function displaySignupMessage(response) {
 const userLogin = document.querySelector("#user-login")
 const userLoginButton = document.querySelector("#user-login-submit")
 const logOutButton = document.querySelector("#user-logout")
-const dealButton = document.querySelector("#deal-button")
+
 const colorChoiceForm = document.querySelector("#color-choice-form")
 const userOptionsSection = document.querySelector("#user-options-section")
 
@@ -93,8 +93,6 @@ function displayLoginMessage(user) {
     } 
 }
 
-
-
 //User Logout
 const documentBody = document.querySelector("body")
 const handsContainer = document.querySelector("#hands-section")
@@ -110,7 +108,7 @@ logOutButton.addEventListener("click", event => {
     colorChoiceForm.style.display = "none" 
     logOutButton.style.display = "none"
     handsContainer.style.display = "none"
-    dealButton.style.display = "block"
+    userOptionsSection.style.display = "none"
 
     //reset cards
     clearHands()
@@ -132,10 +130,47 @@ backgroundColorForm.addEventListener("submit", event => {
     documentBody.style["backgroundColor"] = color
 })
 
+const dealButton = document.querySelector("#deal-button")
+const playButton = document.querySelector("#play-button")
+const foldButton = document.querySelector("#fold-button")
+const quitButton = document.querySelector("#quit-button")
+
+//get player's bets
+const betsForm = document.querySelector('#bets-form')
+const betsFormSubmit = document.querySelector('#bets-submit')
+const purseValue = document.querySelector('#purse span')
+const pairPlusValue = document.querySelector('#pair-plus span')
+const anteValue = document.querySelector('#ante span')
+
+let purse = 1000;
+purseValue.textContent = purse;
+
+betsForm.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const formData = new FormData(event.target)
+    const pairPlus = formData.get("pairPlus")
+    const ante = formData.get("ante")
+
+    //update purse
+    purse = purse - pairPlus - ante;
+    purseValue.textContent = purse;
+
+    //display pair plus bet and ante bet
+    pairPlusValue.textContent = pairPlus;
+    anteValue.textContent = ante;
+
+    //hide bet button and cashout button
+    quitButton.style.display = 'none'
+    betsForm.style.display = 'none'
+})
+
 //deal player's hand
 const playersHandContainer = document.querySelector("#players-hand-container")
 const playersHandDescription = document.querySelector("#players-hand-description")
+
 const deckURL = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+
 let deckId
 let playersHandValues = []
 let dealersHandValues = []
@@ -150,8 +185,10 @@ dealButton.addEventListener("click", event => {
     event.preventDefault()
     clearHands()
     
-    //hide play button
     dealButton.style.display = "none"
+    quitButton.style.display = "none"
+    playButton.style.display = "flex"
+    foldButton.style.display = "flex"
 
     //display hands container
     handsContainer.style.display = "flex"
@@ -168,6 +205,26 @@ dealButton.addEventListener("click", event => {
         .then(() => {
             handInfo = appendHandDescription(playersHandValues)   
         })
+        .then(() => {
+            purse = purse + pairPlusPayout(pairPlusValue.textContent, playersHandValues)
+            purseValue.textContent = purse;
+        })
+})
+
+foldButton.addEventListener('click', event => {
+    event.preventDefault()
+
+    clearHands()
+
+    handsContainer.style.display = "none"
+    foldButton.style.display = "none"
+    playButton.style.display = "none"
+    dealButton.style.display = "flex"
+    quitButton.style.display = "flex"
+
+    betsForm.style.display = "flex"
+    pairPlusValue.textContent = "";
+    anteValue.textContent = "";
 })
 
 function clearHands() {
@@ -191,27 +248,27 @@ function appendHandDescription(hand) {
 
     if(isPair(hand)) {
         [type, value, other] = handValue(hand)
-        playersHandDescription.textContent =`Pair of ${handValueToType(value)} !`
+        playersHandDescription.textContent =`Pair of ${handValueToType(value)}. You win ${pairPlusPayout(pairPlusValue.textContent, hand)} !`
         return [type, value, other]
     } else if(isStraightFlush(hand)) {
         [type, value] = handValue(hand)
-        playersHandDescription.textContent =`WOO HOO STRAIGHT FLUSH ${handValueToType(value)} HIGH !!!!`
+        playersHandDescription.textContent =`WOO HOO STRAIGHT FLUSH ${handValueToType(value)} HIGH, WIN ${pairPlusPayout(pairPlusValue.textContent, hand)}!!!!`
         return [type, value]
     } else if(isStraight(hand)) {
         [type, value] = handValue(hand)
-        playersHandDescription.textContent =`WOO ${handValueToType(value)} HIGH STRAIGHT !!`
+        playersHandDescription.textContent =`WOO ${handValueToType(value)} HIGH STRAIGHT, WIN ${pairPlusPayout(pairPlusValue.textContent, hand)} !!`
         return [type, value]
     } else if(isFlush(hand)) {
         [type, value] = handValue(hand)
-        playersHandDescription.textContent =`WOO ${handValueToType(value)} HIGH FLUSH !!`
+        playersHandDescription.textContent =`WOO ${handValueToType(value)} HIGH FLUSH, WIN ${pairPlusPayout(pairPlusValue.textContent, hand)} !!`
         return [type, value]
     } else if(isThreeOfKind(hand)) {
         [type, value] = handValue(hand)
-        playersHandDescription.textContent =`WOO SET OF ${handValueToType(value)}s !!!`
+        playersHandDescription.textContent =`WOO SET OF ${handValueToType(value)}s, WIN ${pairPlusPayout(pairPlusValue.textContent, hand)}!!!`
         return [type, value]
     } else {
         [type, value] = handValue(hand)
-        playersHandDescription.textContent =`AH ${handValueToType(value)} HIGH...`
+        playersHandDescription.textContent =`Ahh ${handValueToType(value)} high...`
         return [type, value]
     }
 }
@@ -304,7 +361,7 @@ function isThreeOfKind(hand) {
 
 function isPair(hand) {
     let sortedValues = sortedHandValues(hand)
-    return (sortedValues[0] === sortedValues[1] || sortedValues[1] === sortedValues[2]) ? true : false
+    return ((sortedValues[0] === sortedValues[1] || sortedValues[1] === sortedValues[2]) && sortedValues[0] !== sortedValues[2]) ? true : false
 }
 
 function pairInfo(hand) {
@@ -321,3 +378,24 @@ function pairInfo(hand) {
 
     return [pairValue, otherValue]
 }
+
+function pairPlusPayout(bet, hand) {
+    let handType = handValue(hand)[0]
+    console.log(handType)
+
+    switch(handType) {
+        case 'Pair':
+            return 2 * bet;
+        case 'Flush':
+            return 5 * bet;
+        case 'Straight':
+            return 6 * bet;
+        case 'Three Of a Kind':
+            return 31 * bet;
+        case 'Straight Flush':
+            return 41 * bet;
+        default:
+            console.log('sup')
+            return 0;
+    }
+} 
